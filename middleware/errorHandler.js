99be5e569979eaa45df3a34d00bab7a8724e2ba4
rafaelbves <60044361+rafaelbves@users.noreply.github.com>
2022-07-18 19:04:16 -0300

    Criação do middleware de erro

---
+++ middleware/errorHandler.js
@@ -0,0 +1,18 @@
+require('express-async-errors');
+
+class HttpError extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+    }
+};
+
+const errorHandler = (err, req, res, next) => {
+    const { status, message } = err;
+    if (!status) return res.status(500).json(message);
+    return res.status(status).json({ message })
+}
+
+module.exports = errorHandler;
+
+
