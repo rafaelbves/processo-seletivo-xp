b7b07517d324ebf4f434538baef2237ce310c5cb
rafaelbves <60044361+rafaelbves@users.noreply.github.com>
2022-07-21 11:42:51 -0300

    criação da camada controller para a rota de ativos e conclusão da implementação da rota /ativo/cliente/:condCliente

--- routes/ativosRouter.js
+++ routes/ativosRouter.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-
 // eslint-disable-next-line new-cap
 const router = Router();
 
-router.get('/cliente/:codCliente' );
+const ativosController = require('../controllers/ativosController');
+
+router.get('/cliente/:codCliente', ativosController.getAssetsByClient);
 router.get('/:codAtivos' );
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const {Router} = require('express');
 const model = require('../models/models');
 
 const investimentosRouter = require('./investimentosRouter');
+const ativosRouter = require('../routes/ativosRouter');
 // eslint-disable-next-line new-cap
 const router = Router();
 
 
 router.use('/investimentos', investimentosRouter);
+router.use('/ativos', ativosRouter);
 router.get('/', async (req, res) => {
   const test = await model.getClientsBalance();
   return res.status(200).json({test});
