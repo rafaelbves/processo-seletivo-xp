162931118a5d8477b6022f32230b1f728d97bd28
rafaelbves <60044361+rafaelbves@users.noreply.github.com>
2022-07-21 10:44:16 -0300

    Adaptações ESLint

--- models/models.js
+++ models/models.js
@@ -1,9 +1,9 @@
 const connection = require('./connection');
 
-const buyOrSellRequest = async(buyOrder) => {
-  const { codCliente, codAtivo, qtdeAtivo } = buyOrder;
+const buyOrSellRequest = async (buyOrder) => {
+  const {codCliente, codAtivo, qtdeAtivo} = buyOrder;
   const [query] = await connection.execute(
-    `
+      `
     INSERT INTO
       XPSel.compras_vendas (id_user, id_ativo, quantidade, data)
     VALUES
@@ -12,38 +12,33 @@ const buyOrSellRequest = async(buyOrder) => {
   );
 
   return query;
-}
+};
 
 const countBalanceMovement = async (newTransaction) => {
-  const { codCliente, valor, transactionType } = newTransaction;
+  const {codCliente, valor, transactionType} = newTransaction;
   const [query] = await connection.execute(
-    `
+      `
     INSERT INTO
       XPSel.contas (id_user, transacao, tipo, data)
-    VALUES
-	    (${codCliente}, ${valor}, '${transactionType}', NOW());
-    `
+    VALUES (${codCliente}, ${valor}, '${transactionType}', NOW());
+    `,
   );
-  
+
   return query;
-}
+};
 
 const getAssetsAvailable = async () => {
   const [query] = await connection.execute(
-    `
-    SELECT 
-	    a.id AS codAtivo,
+      `
+    SELECT a.id AS codAtivo,
       a.quantidade - IFNULL(SUM(cv.quantidade), 0) AS qtdeAtivo,
       a.valor AS valor
-    FROM 
-	    XPSel.compras_vendas AS cv
-    RIGHT JOIN 
-	    XPSel.ativos as a
-    ON 
-	    cv.id_ativo = a.id
+    FROM XPSel.compras_vendas AS cv
+    RIGHT JOIN XPSel.ativos as a
+    ON cv.id_ativo = a.id
     GROUP BY
       a.id;
-    `
+    `,
   );
 
   return query;
@@ -51,7 +46,7 @@ const getAssetsAvailable = async () => {
 
 const getClientsAssets = async () => {
   const [query] = await connection.execute(
-    `
+      `
     SELECT 
     cv.id_user AS codCliente,
       a.id AS codAtivo,
@@ -66,15 +61,15 @@ const getClientsAssets = async () => {
     GROUP BY
       a.id, 
       cv.id_user;
-    `     
+    `,
   );
 
   return query;
-}
+};
 
 const getClientsBalance = async () => {
   const [query] = await connection.execute(
-    `
+      `
     SELECT 
     id_user AS codCliente,
       sum(transacao) AS saldo
@@ -82,7 +77,7 @@ const getClientsBalance = async () => {
       XPSel.contas
     GROUP BY
       id_user;
-    `
+    `,
   );
 
   return query;
@@ -90,16 +85,16 @@ const getClientsBalance = async () => {
 
 const undoAction = async (table, id) => {
   const [query] = await connection.execute(
-    `
+      `
     DELETE FROM
       XPSel.${table}
     WHERE
       id = ${id};
-    `
-  )
-  
+    `,
+  );
+
   return query;
-}
+};
 
 module.exports = {
   buyOrSellRequest,
@@ -108,4 +103,4 @@ module.exports = {
   getClientsAssets,
   getClientsBalance,
   undoAction,
-}
+};
