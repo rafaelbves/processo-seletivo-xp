458c898fac81c735a0e6476249b00fb03a24775f
rafaelbves <60044361+rafaelbves@users.noreply.github.com>
2022-07-21 10:43:40 -0300

    mais adaptações ESLint

--- controllers/investimentosController.js
+++ controllers/investimentosController.js
@@ -1,30 +1,26 @@
 const investimentosService = require('../services/investimentosService');
 
 const postBuyOrder = async (req, res, next) => {
-  const { body } = req;
+  const {body} = req;
   try {
     const {status, message} = await investimentosService.postBuyOrder(body);
-    return res.status(status).json({ message });
-
+    return res.status(status).json({message});
   } catch (err) {
-    next(err)
+    next(err);
   }
-  
 };
 
 const postSellOrder = async (req, res, next) => {
-  const { body } = req;
+  const {body} = req;
   try {
     const {status, message} = await investimentosService.postSellOrder(body);
-    return res.status(status).json({ message });
-
+    return res.status(status).json({message});
   } catch (err) {
-    next(err)
+    next(err);
   }
-  
 };
 
 module.exports = {
   postBuyOrder,
   postSellOrder,
-}
+};
